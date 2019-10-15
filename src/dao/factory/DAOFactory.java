14701fbd0079c5e65610a5fd31c5868b2c958365
Alexei Bobko <41486012+Abvbobko@users.noreply.github.com>
2019-10-15 17:25:44 +0300

    Realise the raw servise layer and modified DAO layer

--- src/dao/factory/DAOFactory.java
+++ src/dao/factory/DAOFactory.java
@@ -3,10 +3,11 @@ package dao;
 import dao.impl.CSVFlightDAO;
 import dao.impl.CSVUserDAO;
 
-public class DAOFactory {
+public final class DAOFactory {
     private static final DAOFactory instance = new DAOFactory();
     private final FlightDAO csvFlightImpl = new CSVFlightDAO();
     private final UserDAO csvUserImpl = new CSVUserDAO();
+
     private DAOFactory(){}
     public static DAOFactory getInstance(){
         return instance;
